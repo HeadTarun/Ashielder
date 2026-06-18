1d4988b10ece27106e14b7c4ab2b5e5044dc2e86
headtarun <tarundangeengineer@gmail.com>
2026-06-18 11:35:43 +0530

    Started Building Agent

---
+++ tri_model_agent/__init__.py
@@ -0,0 +1,3 @@
+from .agent import fallback_agent, root_agent
+
+__all__ = ["fallback_agent", "root_agent"]
---
+++ tri_model_agent/agent.py
@@ -0,0 +1,3 @@
+from main import fallback_agent, root_agent
+
+__all__ = ["fallback_agent", "root_agent"]
